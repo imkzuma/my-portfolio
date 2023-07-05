8e5683e333d5e94ea7d9da54fa5506ba2c157ee2
imkzuma <edu.gungkrisna@gmail.com>
2023-07-05 11:20:23 +0800

    [ADDED] Added mobile navbar dashboard

--- components/dashboardNav/Navbar.tsx
+++ components/dashboardNav/Navbar.tsx
@@ -1,7 +1,69 @@
-import { Flex, useColorModeValue, useColorMode, Button, Menu, MenuButton, MenuList, MenuGroup, MenuDivider, MenuItem, Avatar, IconButton, Stack, HStack, Switch, Text, Box, useDisclosure } from "@chakra-ui/react";
+import { Flex, useColorModeValue, useColorMode, Button, Menu, MenuButton, MenuList, MenuGroup, MenuDivider, MenuItem, Avatar, IconButton, Stack, HStack, Switch, Text, Box, useDisclosure, Divider, Drawer, DrawerBody, DrawerContent, DrawerHeader, CloseButton } from "@chakra-ui/react";
 import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
 import { Logo } from "../logo";
+
+
+const LinkParent = ({ path, name }: { path: string, name: string }) => {
+  const router = useRouter();
+  const activeColor = useColorModeValue('blue.500', 'blue.500');
+  const inactiveColor = useColorModeValue('gray.400', 'gray.500');
+
+  return (
+    <Text as="a"
+      href={path}
+      w={'fit-content'}
+      color={
+        router.pathname === path
+          ? activeColor
+          : inactiveColor
+      }
+    >
+      {name}
+    </Text>
+  )
+}
+
+const MobileNav = ({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) => {
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} placement="left" size={'full'}>
+      <DrawerContent display={{ md: 'none' }}>
+        <DrawerHeader borderBottomWidth='1px'>
+          <Flex alignItems={'center'} justifyContent={'space-between'}>
+            <Text color={'blue.400'}>
+              <Flex alignItems={'center'} gap={2} fontWeight={'semibold'}>
+                <Logo py={2} />
+                Portfolio.
+              </Flex>
+            </Text>
+            <Button onClick={onClose} variant={'unstyled'}>
+              <CloseButton />
+            </Button>
+          </Flex>
+        </DrawerHeader>
+        <DrawerBody>
+          <Stack spacing={5} py={8}>
+            <Text fontWeight={'semibold'} color={useColorModeValue('gray.600', 'gray.400')}>Dashboard</Text>
+            <Stack spacing={3}>
+              <LinkParent path="/dashboard" name="Home" />
+            </Stack>
+            <Divider w={'70%'} />
+            <Text fontWeight={'semibold'} color={useColorModeValue('gray.600', 'gray.400')}>Blog</Text>
+            <Stack spacing={3}>
+              <LinkParent path="/dashboard/blog" name="Blog List" />
+              <LinkParent path="/dashboard/blog/create" name="Create Blog" />
+            </Stack>
+            <Divider w={'70%'} />
+            <Text fontWeight={'semibold'} color={useColorModeValue('gray.600', 'gray.400')}>Profile</Text>
+            <Stack spacing={3}>
+              <LinkParent path="/dashboard/profile" name="My Profile" />
+            </Stack>
+          </Stack >
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer >
+  )
+}
 
 export default function Navbar() {
   const router = useRouter();
@@ -23,6 +85,7 @@ export default function Navbar() {
       align={'center'}
       justify={'space-between'}
     >
+      <MobileNav isOpen={isOpen} onClose={onClose} />
       <HStack spacing={5}>
         <IconButton
           icon={isOpen ? <CloseIcon fontSize={'xl'} /> : <HamburgerIcon fontSize={'xl'} />}
